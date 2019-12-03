8b5c13b5d3d60572289be3760998963849c39e74
Thomas Weber <tommyweber33@gmail.com>
2019-12-02 20:46:05 -0600

    Fix private config

--- src/config.js
+++ src/config.js
@@ -1,16 +1,32 @@
-// To change the config, edit config.private.js.
+// To change the config, edit config.private.js in the repository root. (created after first run)
 // To change the default values, edit config.default.js
 
+const fs = require('fs');
+const path = require('path');
 const utils = require('./utils');
 const logger = require('./logger');
 
-const defaults = require('./config.default');
-// @ts-ignore
+const defaultConfig = require('./config.default');
+const privateConfigPath = path.join(__dirname, '..', 'config.private.js');
+
+// Create the private config file, if it is missing.
 try {
-  const private = require('./config.private');
-  utils.deepMerge(defaults, private);
+  fs.lstatSync(privateConfigPath);
 } catch (e) {
-  logger.error('cannot read private settings', e);
+  logger.debug('creating private config file');
+  try {
+    fs.writeFileSync(privateConfigPath, '');
+  } catch (e) {
+    logger.error('cannot create private config', e);
+  }
 }
 
-module.exports = defaults;
+// Attempt to load the private config
+try {
+  const private = require('../config.private');
+  utils.deepMerge(defaultConfig, private);
+} catch (e) {
+  logger.error('cannot read private config', e);
+}
+
+module.exports = defaultConfig;
--- src/config.private.js
+++
@@ -1,3 +0,0 @@
-module.exports.APP = {
-  port: 8081,
-}
