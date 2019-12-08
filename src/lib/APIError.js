ccf5b156f0c307469ccebc3b68e8e9b11b321f38
Thomas Weber <tommyweber33@gmail.com>
2019-12-08 14:13:36 -0600

    Update APIError jsdoc

--- src/lib/APIError.js
+++ src/lib/APIError.js
@@ -1,7 +1,8 @@
 class APIError extends Error {
   /**
-   * @param {string} code
-   * @param {string} message
+   * @param {string} code Type of error.
+   * @param {number} status HTTP status code for the error.
+   * @param {string} message The error message, which may be shown to the user.
    */
   constructor(code, status, message) {
     super(`${message}`);
@@ -17,18 +18,22 @@ APIError.InternalError = class extends APIError {
 };
 /** An error that indicates too many requests are being made in total or by an individual user */
 APIError.TooManyRequests = class extends APIError {
+  /** @param {string} message */
   constructor(message) { super('TOO_MANY_REQUESTS', 429, message); }
 };
 /** An error that indicates there is something wrong with the request */
 APIError.BadRequest = class extends APIError {
+  /** @param {string} message */
   constructor(message) { super('BAD_REQUEST', 400, message); }
 };
 /** An error that indicates that the item asked for does not exist */
 APIError.NotFound = class extends APIError {
+  /** @param {string} message */
   constructor(message) { super('NOT_FOUND', 404, message); }
 };
 /** An error that indicates that an upstream API returned a strange response */
 APIError.UpstreamError = class extends APIError {
+  /** @param {string} message */
   constructor(message) { super('UPSTREAM_ERROR', 500, message); }
 };
 
