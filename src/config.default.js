7e7da804c4b284b5eff8160a0f807802ed90dc1d
Thomas Weber <tommyweber33@gmail.com>
2020-02-18 16:27:10 -0600

    Tune caching TTL/maxEntries

--- src/config.default.js
+++ src/config.default.js
@@ -1,21 +1,26 @@
 // This is the default configuration.
 // To make private changes, edit config.private.js in the parent directory (automatically created after first launch)
 
+// These are some helpful constants you may want in your private config.
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * MINUTE * 60;
+
 module.exports.API_WRAPPER = {
   enabled: true,
-  projectCache: { name: 'project-meta', ttl: 1000 * 60 * 60, maxEntries: 500 },
-  userCache: { name: 'user-meta', ttl: 1000 * 60 * 60, maxEntries: 500 },
-  studioCache: { name: 'studio-meta', ttl: 1000 * 60 * 60, maxEntries: 500 },
+  projectCache: { name: 'project-meta', ttl: HOUR * 6, maxEntries: 1000 },
+  userCache: { name: 'user-meta', ttl: HOUR, maxEntries: 50 },
+  studioCache: { name: 'studio-meta', ttl: HOUR, maxEntries: 100 },
 };
 
 module.exports.SITE_API_WRAPPER = {
   enabled: true,
-  studioPageCache: { name: 'studio-pages', ttl: 1000 * 60 * 60, maxEntries: 100 },
+  studioPageCache: { name: 'studio-pages', ttl: HOUR, maxEntries: 100 },
 };
 
 module.exports.CLOUD_WRAPPER = {
   enabled: true,
-  logCache: { name: 'cloud-history', ttl: 1000 * 60, maxEntries: 50 },
+  logCache: { name: 'cloud-history', ttl: MINUTE * 30, maxEntries: 100 },
 };
 
 module.exports.APP = {
