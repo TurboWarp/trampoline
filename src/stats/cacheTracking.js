e8b2c0dcc3f198b8062444905de82d2f3d98d6d8
Thomas Weber <weber_t@mailbox.org>
2021-01-03 15:26:38 -0600

    Log error counts

--- src/stats/cacheTracking.js
+++ src/stats/cacheTracking.js
@@ -12,11 +12,19 @@ function printCacheInfo() {
     let fresh = 0;
     let stale = 0;
     let unseen = 0;
+    let ok = 0;
+    let error = 0;
     for (const entry of cache.entries) {
       if (cache.isExpired(entry)) {
         stale++;
       } else {
         fresh++;
+      }
+
+      if (entry.error) {
+        error++;
+      } else {
+        ok++;
       }
 
       if (!seenCacheEntries.has(entry)) {
@@ -26,6 +34,7 @@ function printCacheInfo() {
     }
 
     logger.info(`fresh: ${fresh}  stale: ${stale}  unseen: ${unseen}`);
+    logger.info(`error: ${error}  ok: ${ok}`);
   }
 }
 
