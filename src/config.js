0fef06aa3c62bef80d4e2843102a3c053d33b8d2
Thomas Weber <weber_t@mailbox.org>
2021-07-09 22:15:47 -0500

    Improve logging

--- src/config.js
+++ src/config.js
@@ -1,19 +1,3 @@
-module.exports.LOGGING = {
-  // console logging is enabled in debug mode (NODE_ENV !== production), disabled in production mode (NODE_ENV === production)
-  // set this to true to forcibly enable the console logging in production mode
-  forceEnableConsoleLogging: false,
-  // these options are passed directly into winston-daily-rotate-file
-  // see https://github.com/winstonjs/winston-daily-rotate-file#options
-  rotation: {
-    filename: '%DATE%.log',
-    // LOGS_DIRECTORY is used by systemd
-    dirname: process.env.LOGS_DIRECTORY || 'logs',
-    datePattern: 'YYYY-MM-DD',
-    maxFiles: '7d',
-    createSymlink: true,
-  },
-};
-
 module.exports.APP = {
   allowOrigins: '*',
   port: process.env.PORT || 8080,
--- src/logger.js
+++ src/logger.js
@@ -1,27 +1,36 @@
 const winston = require('winston');
 require('winston-daily-rotate-file');
 
-const config = require('./config');
 const environment = require('./environment');
+
+const format = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.splat(),
+  winston.format.printf(({ level, message, timestamp }) => {
+    return `[${timestamp}] ${level}: ${message}`;
+  })
+);
 
 const logger = winston.createLogger({
   level: environment.isDevelopment ? 'debug' : 'info',
-  format: winston.format.combine(
-    winston.format.splat(),
-    winston.format.simple()
-  ),
+  format: format
 });
 
-logger.add(new winston.transports.DailyRotateFile(config.LOGGING.rotation));
+logger.add(new winston.transports.DailyRotateFile({
+  filename: '%DATE%.log',
+  // LOGS_DIRECTORY is used by systemd services with the LogsDirectory= directive
+  dirname: process.env.LOGS_DIRECTORY || 'logs',
+  datePattern: 'YYYY-MM-DD',
+  maxFiles: '7d',
+  createSymlink: true,
+}));
 
-if ((environment.isDevelopment || config.LOGGING.forceEnableConsoleLogging) && !environment.isTest) {
-  logger.add(new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple(),
-    ),
-  }));
-  logger.debug('Development mode');
-}
+logger.add(new winston.transports.Console({
+  format: winston.format.combine(
+    // Include color when logging to console, not when logging to file (results in ugly escape codes)
+    winston.format.colorize(),
+    format
+  )
+}));
 
 module.exports = logger;
