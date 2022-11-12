17f1e3be54782d24743029110e22ce4a3799147f
GarboMuffin <muffin@mailbox.org>
2022-11-12 16:40:44 -0600

    Add a utility script for testing project tokens

---
+++ tools/token-prober.js
@@ -0,0 +1,53 @@
+/**
+ * @fileoverview Utility script to probe a project API endpoint and extract information about the
+ * timestamps in the project token like the embedded timestamps and whether or not the token
+ * actually works.
+ */
+
+const http = require('http');
+const https = require('https');
+const fetch = require('node-fetch').default;
+
+const ID = '10128407';
+
+const SCRATCH_API = 'https://api.scratch.mit.edu/projects/$id';
+const TRAMPOLINE_API = 'https://trampoline.turbowarp.org/proxy/projects/$id';
+const LOCALHOST_API = 'http://localhost:8080/proxy/projects/$id';
+
+const run = async (url) => {
+  console.log(`Using ${url} with ID ${ID}`);
+
+  const agent = new (url.startsWith('https:') ? https : http).Agent({
+    keepAlive: true
+  });
+  
+  while (true) {
+    const apiResponse = await fetch(url.replace('$id', ID).replace('$random', Math.random()), {
+      agent
+    });
+    if (!apiResponse.ok) {
+      throw new Error(`Could not get metadata: ${apiResponse.status}`);
+    }
+    const json = await apiResponse.json();
+    const token = json.project_token;
+    const tokenTimestamp = +token.split('_')[0];
+    const now = Math.round(Date.now() / 1000);
+    const delta = (tokenTimestamp - now).toString().padStart(3, ' ');
+    process.stdout.write(`${tokenTimestamp} - ${now} = ${delta}${delta < 0 ? ' !!! expired !!!' : ''} ... `);
+
+    const projectResponse = await fetch(`https://projects.scratch.mit.edu/${ID}?token=${token}`);
+    if (projectResponse.ok) {
+      process.stdout.write(`OK\n`);
+    } else {
+      process.stdout.write(`ERROR: ${projectResponse.status}`);
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  }
+};
+
+run(SCRATCH_API)
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
